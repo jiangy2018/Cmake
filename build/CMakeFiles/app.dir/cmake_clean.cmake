2ecc80a1d0d787722f517ca89468a247d4ef932f
jiangy <jiangy1977@outlook.com>
2024-11-13 14:22:54 +0800

    create new

---
+++ build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/Users/jiangy/Desktop/Project/C++/Learning_Source/Cmake/bin/app.exe"
+  "C:/Users/jiangy/Desktop/Project/C++/Learning_Source/Cmake/bin/app.exe.manifest"
+  "C:/Users/jiangy/Desktop/Project/C++/Learning_Source/Cmake/bin/app.pdb"
+  "C:/Users/jiangy/Desktop/Project/C++/Learning_Source/Cmake/bin/libapp.dll.a"
+  "CMakeFiles/app.dir/src/test.c.obj"
+  "CMakeFiles/app.dir/src/test.c.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/app.dir/src/test.c.obj.d
@@ -0,0 +1,16 @@
+CMakeFiles/app.dir/src/test.c.obj: \
+ C:\Users\jiangy\Desktop\Project\C++\Learning_Source\Cmake\src\test.c \
+ C:/PROGRA~1/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/PROGRA~1/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/PROGRA~1/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/PROGRA~1/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/PROGRA~1/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/PROGRA~1/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/PROGRA~1/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ C:/PROGRA~1/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/PROGRA~1/mingw64/x86_64-w64-mingw32/include/_mingw_print_push.h \
+ C:/PROGRA~1/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/PROGRA~1/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/PROGRA~1/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/PROGRA~1/mingw64/x86_64-w64-mingw32/include/_mingw_print_pop.h \
+ C:/Users/jiangy/Desktop/Project/C++/Learning_Source/Cmake/include/mymath.h
